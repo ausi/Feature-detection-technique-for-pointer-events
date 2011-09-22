c051afa38bf9f3b9def30fc3b7d0dfbddaff7b3b
ausi <martin@auswoeger.com>
2011-09-22 23:19:34 +0200

    replaced tabs by spaces

--- README.md
+++ README.md
@@ -4,15 +4,15 @@ Modernizr plugin feature detection for css property "pointer-events"
 How to use
 ----------
 
-	<script src="modernizr-pointerevents.min.js"></script>
-	<script>
-		if(Modernizr.pointerevents){
-			alert('pointer events are supported :)');
-		}
-		else{
-			alert('pointer events are NOT supported!');
-		}
-	</script>
+    <script src="modernizr-pointerevents.min.js"></script>
+    <script>
+        if(Modernizr.pointerevents){
+            alert('pointer events are supported :)');
+        }
+        else{
+            alert('pointer events are NOT supported!');
+        }
+    </script>
 
 Example
 -------
--- modernizr-pointerevents.js
+++ modernizr-pointerevents.js
@@ -1,15 +1,15 @@
 Modernizr.addTest('pointerevents', function(){
     var element = document.createElement('script'),
-		supports;
+        supports;
     if(!('pointerEvents' in element.style)){
         return false;
     }
     element.style.pointerEvents = 'auto';
     element.style.pointerEvents = 'x';
     document.documentElement.appendChild(element);
-	supports = document.defaultView && 
-		document.defaultView.getComputedStyle && 
-		document.defaultView.getComputedStyle(element, '').getPropertyValue('pointer-events') === 'auto';
+    supports = document.defaultView && 
+        document.defaultView.getComputedStyle && 
+        document.defaultView.getComputedStyle(element, '').getPropertyValue('pointer-events') === 'auto';
     document.documentElement.removeChild(element);
     return supports;
 });
