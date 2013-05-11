88480f4fe381926898ccbc365c0cb4d392f2f277
Martin Auswöger <martin@auswoeger.com>
2013-05-11 20:04:17 +0200

    Improved the detection method

--- modernizr-pointerevents.js
+++ modernizr-pointerevents.js
@@ -1,16 +1,5 @@
 Modernizr.addTest('pointerevents', function(){
-    var element = document.createElement('x'),
-        documentElement = document.documentElement,
-        getComputedStyle = window.getComputedStyle,
-        supports;
-    if(!('pointerEvents' in element.style)){
-        return false;
-    }
-    element.style.pointerEvents = 'auto';
-    element.style.pointerEvents = 'x';
-    documentElement.appendChild(element);
-    supports = getComputedStyle && 
-        getComputedStyle(element, '').pointerEvents === 'auto';
-    documentElement.removeChild(element);
-    return !!supports;
+	var element = document.createElement('x');
+	element.style.cssText = 'pointer-events:auto';
+	return element.style.pointerEvents === 'auto';
 });
